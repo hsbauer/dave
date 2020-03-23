7057ebed336cb3e1fd7f86165e11a02633840239
Scott Bauer <bauer.scott@mayo.edu>
2020-03-23 15:33:21 -0500

    initial commit

---
+++ src/main/java/dave/org/dave/DaveApplication.java
@@ -0,0 +1,13 @@
+package dave.org.dave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaveApplication.class, args);
+	}
+
+}
